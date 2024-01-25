846572246a28be3ed0b71d9df13d1b6a0d430e85
Sheroze <154746371+estirner@users.noreply.github.com>
2024-01-25 21:30:24 +0500

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -2,8 +2,15 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    while True:
+        client_socket, addr = server_socket.accept()
+        print(f"Connection from {addr} has been established.")
+        request = client_socket.recv(1024)
+        print('Received request:')
+        print(request.decode('utf-8'))
+        response = 'HTTP/1.1 200 OK\r\n\r\n'
+        client_socket.send(bytes(response, 'utf-8'))
+        client_socket.close()
 
 if __name__ == "__main__":
     main()
