973dd08b9fa040da0bdbe8e268b4b7e72008cfb0
Sheroze <154746371+estirner@users.noreply.github.com>
2024-01-28 23:30:41 +0500

    pass seventh stage

--- app/main.py
+++ app/main.py
@@ -10,34 +10,27 @@ def handle_client(client_socket, directory):
     print(f'Received request for path: {path}')
     if path == '/':
         response = 'HTTP/1.1 200 OK\r\n\r\n'
-    elif path.startswith('/echo/'):
-        response_body = path[6:]
-        response_headers = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(response_body)}\r\n\r\n'
-        response = response_headers + response_body
-    elif path == '/user-agent':
-        user_agent = next((line.split(': ')[1] for line in request_lines[1:] if line.lower().startswith('user-agent')), None)
-        if user_agent:
-            response_headers = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n'
-            response = response_headers + user_agent
-        else:
-            response = 'HTTP/1.1 400 Bad Request\r\n\r\n'
     elif path.startswith('/files/'):
-        filename = os.path.join(directory, path[7:])
-        if os.path.isfile(filename):
-            with open(filename, 'rb') as file:
-                response_body = file.read()
-            response_headers = f'HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(response_body)}\r\n\r\n'
-            response = response_headers.encode('utf-8') + response_body
+        if directory is None:
+            response = 'HTTP/1.1 400 Bad Request\r\n\r\n'
         else:
-            response = 'HTTP/1.1 404 Not Found\r\n\r\n'.encode('utf-8')
+            filename = path[7:]
+            file_path = os.path.join(directory, filename)
+            if os.path.exists(file_path):
+                with open(file_path, 'rb') as file:
+                    file_content = file.read()
+                response_headers = f'HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(file_content)}\r\n\r\n'
+                response = bytes(response_headers, 'utf-8') + file_content
+            else:
+                response = 'HTTP/1.1 404 Not Found\r\n\r\n'
     else:
-        response = 'HTTP/1.1 404 Not Found\r\n\r\n'.encode('utf-8')
-    client_socket.send(response)
+        response = 'HTTP/1.1 404 Not Found\r\n\r\n'
+    client_socket.send(bytes(response, 'utf-8') if isinstance(response, str) else response)
     client_socket.close()
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--directory', type=str, default=os.getcwd())
+    parser.add_argument('--directory')
     args = parser.parse_args()
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
